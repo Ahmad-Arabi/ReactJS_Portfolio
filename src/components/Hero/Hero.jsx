b9a0de77e325b6f149094c05384417ab7b983986
Ahmnad-Arabi <177164340+Ahmad-Arabi@users.noreply.github.com>
2025-04-22 00:32:01 +0300

    Fixed the svg name in the Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import github from '../../assets/images/github.svg'
+import github from '../../assets/images/gitHub.svg'
 import profilePic from '../../assets/images/profile.png'
 import './style.css'
 
