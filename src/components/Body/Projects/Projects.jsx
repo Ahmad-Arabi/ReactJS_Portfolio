ac80820668b98a1ba54c045414760d4d95176e18
Ahmnad-Arabi <177164340+Ahmad-Arabi@users.noreply.github.com>
2025-04-22 21:55:38 +0300

    Fixed the props for the project name

--- src/components/Body/Projects/Projects.jsx
+++ src/components/Body/Projects/Projects.jsx
@@ -10,24 +10,24 @@ const Projects = () => {
       </article>
       <article className="projects_list">
         <ProjectCard
-          ProjectName="Landing Page Template"
+          projectName="Landing Page Template"
           brief="Demonstration on how to implement HTML & CSS to create a landing page for a website (Project Odin)"
           github="https://github.com/Ahmad-Arabi/landing-page-odin"
           preview="https://ahmad-arabi.github.io/landing-page-odin/"
         />
         <ProjectCard
-          ProjectName="Workout Tracking Website:"
+          projectName="Workout Tracking Website:"
           brief="A fitness tracking website using HTML, CSS, Bootstrap, and JavaScript, allowing users to log and monitor their workout routines (Orange Coding Academey)"
           github="https://github.com/Ahmad-Arabi/group1/tree/main"
           preview="https://tuqajr.github.io/group1/"
         />
         <ProjectCard
-          ProjectName="E-Commerce Website (Carftify)"
+          projectName="E-Commerce Website (Carftify)"
           brief="Led as Scrum Master in a PHP and MySQL-based project. Managed a team to develop a web application using HTML, CSS, JavaScript, and PHP. Ensured agile workflow, coordinated team meetings, and maintained smooth project progression (Orange Coding Academey)"
           github="https://github.com/Ahmad-Arabi/php_project_group_2"
         />
         <ProjectCard
-          ProjectName="Servecies Website (JoHotels)"
+          projectName="Servecies Website (JoHotels)"
           brief="Worked with a team to build a hotel booking website with a fully functional admin dashboard using Laravel (Orange Coding Academey)"
           github="https://github.com/Ahmad-Arabi/Laravel_Project"
         />
